474b2aa835108d8cac8ca9fce609714db3e8098d
dipranil <dipranilchakraborty2010@gmail.com>
2023-06-27 16:22:35 +0530

    added headers

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
+# for windows
+# header = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+#     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+# }
 
+# for mac
 header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
 
